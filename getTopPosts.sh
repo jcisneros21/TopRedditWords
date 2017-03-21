3e74047878be21c0cb9541ff1bde1b56bc428625
jcisneros21 <jcisneros@slu.edu>
2017-03-21 11:01:50 -0500

    Added HTML scripting

--- getTopPosts.sh
+++ getTopPosts.sh
@@ -10,5 +10,21 @@ find $COMDIR -type f -exec cat {} + | egrep -o '<li class=\"first\"><a href=\"[^
 
 rm "$COMDIR/index.html"
 
-find $COMDIR -type f -exec cat {} + | egrep -o '<div class="md"><p>[^<]*</p>' | sed 's/^<div class="md"><p>//' | sed 's/<\/p>$//' | egrep -o '[^ ]+' | sort | uniq -c | sort -r -n | head -n 20
+TIMES=$(find $COMDIR -type f -exec cat {} + | egrep -o '<div class="md"><p>[^<]*</p>' | sed 's/^<div class="md"><p>//' | sed 's/<\/p>$//' | egrep -o '[^ ]+' | sort | uniq -c | sort -r -n | egrep '(Trump|trump)' | sed 's/\s*//' | sed 's/ .*//' | head -n 1)
 
+echo $TIMES
+
+rm -f text.html
+
+touch text.html
+
+echo "<HTML>" >> text.html
+echo "  <HEAD>" >> text.html
+echo "    <TITLE>" >> text.html
+echo "      Reddit's Trump Obsession" >> text.html
+echo "    </TITLE>" >> text.html
+echo "  <BODY>" >> text.html
+echo "    <H3> The number of times trump has been mentioned is: </H3> " >> text.html
+echo "    <p>$TIMES</p>" >> text.html
+echo "  </BODY>" >> text.html
+echo "</HTML>" >> text.html
