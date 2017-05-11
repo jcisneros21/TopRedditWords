8451b05623afea5524f75909ce620bb1215aa335
jcisneros21 <jcisneros@slu.edu>
2017-05-10 22:36:45 -0500

    Added Commits and README.md

--- Python/topComments.py
+++ Python/topComments.py
@@ -1,52 +1,57 @@
-# username = ExtractAccount
-# password = reddit12345
-
 import csv
 import praw
 import json
 
+# Credentials to access the Reddit API
 reddit = praw.Reddit(client_id='vgr0He3IMlSqVA',
                      client_secret='h0WAu-6F6cQsx3Ku0DcC9gtld24',
                      user_agent='by /u/ExtractAccount',
                      username='ExtractAccount',
                      password='reddit12345')
 
+# Extract only 25 post from the front page
 topPosts = []
 for submission in reddit.front.hot(limit=25):
   topPosts.append(submission)
 
+# Lists for comments and information about them
 donaldComments = []
 authorList = []
 comment_ranking = []
+
+# Extract all comments from the front page submissions
 for sub in topPosts:
   submission = reddit.submission(id=sub)
   submission.comments.replace_more(limit=0)
   for comment in submission.comments.list():
     strip_comment = comment.body.replace("\n\n"," ")
     strip_comment = strip_comment.replace("\n", " ")
-    if ' Donald ' in comment.body:
+    if ' Donald ' or ' Donald\'s ' in comment.body:
+      if 'Glover' not in comment.body:
+        authorList.append(comment.author.name)
+        donaldComments.append(strip_comment)
+        comment_ranking.append(comment.score)
+    elif ' donald ' or ' donald\'s ' in comment.body:
+      if 'Glover' not in comment.body:
+        authorList.append(comment.author.name)
+        donaldComments.append(strip_comment)
+        comment_ranking.append(comment.score)
+    elif 'Trump' in comment.body:
       authorList.append(comment.author.name)
       donaldComments.append(strip_comment)
       comment_ranking.append(comment.score)
-    elif ' donald ' in comment.body:
+    elif 'trump' in comment.body:
       authorList.append(comment.author.name)
       donaldComments.append(strip_comment)
       comment_ranking.append(comment.score)
-    elif ' Trump ' in comment.body:
-      authorList.append(comment.author.name)
-      donaldComments.append(strip_comment)
-      comment_ranking.append(comment.score)
-    elif ' trump ' in comment.body:
-      authorList.append(comment.author.name)
-      donaldComments.append(strip_comment)
-      comment_ranking.append(comment.score)
-    # trump's
-    # donald's
 
+# A list of indexes
 index_list = []
 for i in range(0,len(comment_ranking)):
   index_list.append(i)
 
+# Sort by ranking of each comment and allow the list of indexes to be changed accordingly
+# to the rankings
 def bubble_sort(items):
   for i in range(len(items)):
     for j in range(len(items)-1-i):
@@ -55,27 +60,28 @@ def bubble_sort(items):
         index_list[j], index_list[j+1] = index_list[j+1], index_list[j]
 
 bubble_sort(comment_ranking)
-print(comment_ranking)
-print(index_list)
 
-# Creates the JS comment file so the browser can show the comments
+# Creates the JS comment file so the browser can present the comments
 with open('../js/randomTrumpText.js','w+') as myfile:
+  # Write a list for the comments
   myfile.write('text_list = [\n')
   for item in donaldComments:
     holder = json.dumps(item)
     myfile.write('              ' + holder + ',\n')
   myfile.write('             ];\n\n')
+  # Write a list for the authors of each comment
   myfile.write('name_list = [\n')
   for name in authorList:
     holder = json.dumps(name)
     myfile.write('              ' + holder + ',\n')
   myfile.write('             ];\n\n')
+  # Write the indexes of the highest rated comments
   myfile.write('rank_list = [\n')
   for i in range(0,5):
     myfile.write('              ' + str(index_list[i]) + ',\n')
   myfile.write('             ];\n\n')
   myfile.write('var amount = text_list.length;\n')
-  myfile.write('$(\"#numberofComments\").text(amount);\n\n')
+  myfile.write('$(\"#numberofComments\").text(amount + " Comments");\n\n')
   for i in range(0,5):
     myfile.write('$("#comment' + str(i+1) + '").text(text_list[rank_list[' + str(i) + ']]);\n')
   myfile.write('\n')
